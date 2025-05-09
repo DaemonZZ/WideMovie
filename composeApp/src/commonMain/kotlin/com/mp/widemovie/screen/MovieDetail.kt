c33b195f0c59ff1e132e8066a803b4c06c7d1389
Thắng <63739201+DaemonZZ@users.noreply.github.com>
2025-05-09 23:43:56 +0700

    Nav for web (#2)

--- composeApp/src/commonMain/kotlin/com/mp/widemovie/screen/MovieDetail.kt
+++ composeApp/src/commonMain/kotlin/com/mp/widemovie/screen/MovieDetail.kt
@@ -1,12 +1,23 @@
 package com.mp.widemovie.screen
 
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 
 data class MovieDetail(val slug: String) : Screen {
+    override val key = "detail/$slug"
     @Composable
     override fun Content() {
-        Text("DetailScreen: $slug")
+        val nav = LocalNavigator.currentOrThrow
+        Button(
+            onClick = {
+                nav.pop()
+            }
+        ) {
+            Text("Back")
+        }
     }
 }
--- composeApp/src/wasmJsMain/kotlin/com/mp/widemovie/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/mp/widemovie/main.kt
@@ -2,15 +2,13 @@ package com.mp.widemovie
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
-import cafe.adriel.voyager.navigator.Navigator
 import com.mp.widemovie.di.initKoin
-import com.mp.widemovie.screen.HomeScreen
 import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     initKoin()
     ComposeViewport(document.body!!) {
-        Navigator(HomeScreen())
+        App()
     }
 }
