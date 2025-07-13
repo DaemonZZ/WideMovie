f3fb72240430af8fd87e0616f2d141782da93f57
Thắng <63739201+DaemonZZ@users.noreply.github.com>
2025-07-13 10:45:08 +0700

    Fix overlap system ui (#29)

--- composeApp/src/androidMain/kotlin/com/mp/widemovie/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/mp/widemovie/MainActivity.kt
@@ -3,8 +3,16 @@ package com.mp.widemovie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import cafe.adriel.voyager.navigator.Navigator
 import com.google.firebase.auth.FirebaseAuth
 import com.mp.widemovie.ui.screen.home.HomeScreen
@@ -12,10 +20,18 @@ import com.mp.widemovie.ui.screen.home.HomeScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val auth = FirebaseAuth.getInstance()
+        auth.signInAnonymously()
         setContent {
-            Navigator(HomeScreen())
-            val auth = FirebaseAuth.getInstance()
-            auth.signInAnonymously()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(WindowInsets.systemBars.asPaddingValues())
+            ) {
+                Navigator(HomeScreen())
+            }
+
         }
     }
 }
